a782148f47ee23e938f348e3b3661c30bbb8b16c
Thibaud BARDIN <thibaud.bardin@gmail.com>
2015-01-06 17:45:48 +0100

    Update

---
+++ README.md
@@ -0,0 +1,20 @@
+POKEMON
+=======
+
+## Tirer au sort 2 pokémons (1 attaquant / 1 défenseur)
+
+## Logique
+
+### On affiche le numéro du round (1...X)
+
+### Attanquant attaque entre 1 et 2 fois de suite
+
+### On vérifie qu'aucun pokémon n'est mort
+
+#### Si un mort => fini (on affiche le vainqueur)
+
+#### Si pas de mort => continue...
+
+### On switche l'attaquant avec le défenseur
+
+### On recommence
--- pokemon.php
+++ pokemon.php
@@ -3,12 +3,51 @@
 require __DIR__.'/vendor/autoload.php';
 
 use Cartman\Init\Pokemon\PokemonFire;
+use Cartman\Init\Pokemon\PokemonWater;
+use Cartman\Init\Pokemon\PokemonPlant;
 
-$pokemon = new PokemonFire();
-
-$pokemon
+$pokemonFire = new PokemonFire();
+$pokemonFire
     ->setName('Salamèche')
     ->setHP(100)
 ;
 
-var_dump($pokemon);
+$pokemonWater = new PokemonWater();
+$pokemonWater
+    ->setName('Carapuce')
+    ->setHP(100)
+;
+
+$pokemonPlant = new PokemonPlant();
+$pokemonPlant
+    ->setName('Bulbizarre')
+    ->setHP(100)
+;
+
+
+$pokemons = [$pokemonWater, $pokemonFire, $pokemonPlant];
+shuffle($pokemons);
+
+$striker    = array_pop($pokemons);
+$goal       = array_pop($pokemons);
+
+var_dump($striker, $goal);
+
+
+
+$attack = mt_rand(5, 20);
+
+if (true === $striker->isTypeWeak($goal->getType()))
+    $attack = (int)ceil($attack / 2);
+
+if (true === $striker->isTypeStrong($goal->getType()))
+    $attack = (int)ceil($attack * 2);
+
+$goal->removeHP($attack);
+
+
+
+
+var_dump('----------------------------');
+
+var_dump($striker, $goal);
