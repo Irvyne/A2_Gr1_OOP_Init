cae5f88a77d7e8fa4ed62bc68ec759865e54528c
Thibaud BARDIN <thibaud.bardin@gmail.com>
2015-01-07 09:35:18 +0100

    Update

--- pokemon.php
+++ pokemon.php
@@ -2,6 +2,7 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use Cartman\Init\Pokemon\Model\PokemonModel;
 use Cartman\Init\Pokemon\PokemonFire;
 use Cartman\Init\Pokemon\PokemonWater;
 use Cartman\Init\Pokemon\PokemonPlant;
@@ -28,26 +29,51 @@ $pokemonPlant
 $pokemons = [$pokemonWater, $pokemonFire, $pokemonPlant];
 shuffle($pokemons);
 
+/** @var PokemonModel $striker */
 $striker    = array_pop($pokemons);
+/** @var PokemonModel $goal */
 $goal       = array_pop($pokemons);
 
-var_dump($striker, $goal);
+/**
+ * Parameters
+ */
+$roundNumber = 1;
+$matchOver   = false;
 
+echo '<h1>'.$striker->getName().' VS '.$goal->getName().'</h1>';
 
+/**
+ * Logic
+ */
+while (false === $matchOver) {
+    echo '<h2>Round n°'.$roundNumber.'</h2>';
 
-$attack = mt_rand(5, 20);
+    $attackNumber = mt_rand(1, 3);
 
-if (true === $striker->isTypeWeak($goal->getType()))
-    $attack = (int)ceil($attack / 2);
+    for ($i = 0; $i < $attackNumber; $i++) {
+        $attackValue = mt_rand(5, 20);
 
-if (true === $striker->isTypeStrong($goal->getType()))
-    $attack = (int)ceil($attack * 2);
+        if ($striker->isTypeWeak($goal->getType()))
+            $attackValue = ceil($attackValue / 2);
 
-$goal->removeHP($attack);
+        if ($striker->isTypeStrong($goal->getType()))
+            $attackValue = ceil($attackValue * 2);
 
+        $goal->removeHP((int)$attackValue);
 
+        echo '<h3>'.$striker->getName().' attacks '.$goal->getName().'. Attack n°'.($i+1).' on '.$attackNumber.' '.$attackValue.' HP removed. '.$goal->getName().' have '.$goal->getHP().'HP left</h3>';
 
+        if (0 === $goal->getHP()) {
+            $matchOver = true;
+            break;
+        }
+    }
 
-var_dump('----------------------------');
+    if (false === $matchOver)
+        list($striker, $goal) = [$goal, $striker];
 
-var_dump($striker, $goal);
+    $roundNumber++;
+}
+
+echo '<h1>'.$striker->getName().' win!</h1>';
+echo '<h4>'.$striker->getHP().'HP left.</h4>';
